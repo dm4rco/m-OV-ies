90edfab298688a62e1f17bf74466b8b224638ce1
dm4rco <mar.daurelio@gmail.com>
2021-12-17 10:47:07 +0100

    initial commit for project setup

---
+++ app/src/main/java/com/daurelio/m_ov_ies/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.daurelio.m_ov_ies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
